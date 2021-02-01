9165165fcca16dd85ab3ca9f9dcc6080a5114d4b
Subhan Mahmood <subhan70m@gmail.com>
2021-02-01 00:27:21 +0000

    added add to playlist method and styles

--- src/components/wrapped.js
+++ src/components/wrapped.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactSwipe from 'react-swipe'
 import superagent from 'superagent'
 
 function getHashParams() {
@@ -11,30 +12,6 @@ function getHashParams() {
     return hashParams;
 }
 
-function getCssValuePrefix()
-{
-    var rtrnVal = '';//default to standard syntax
-    var prefixes = ['-o-', '-ms-', '-moz-', '-webkit-'];
-
-    // Create a temporary DOM object for testing
-    var dom = document.createElement('div');
-
-    for (var i = 0; i < prefixes.length; i++)
-    {
-        // Attempt to set the style
-        dom.style.background = prefixes[i] + 'linear-gradient(#000000, #ffffff)';
-
-        // Detect if the style was successfully set
-        if (dom.style.background)
-        {
-            rtrnVal = prefixes[i];
-        }
-    }
-
-    dom = null;
-    console.log(rtrnVal)
-    return rtrnVal;
-}
 
 class Callback extends React.Component {
     constructor(props) {
@@ -86,6 +63,7 @@ class Callback extends React.Component {
                 "linear-gradient(0.03deg, #CC95C0 1.38%, #DBD4B4 50.68%, #7AA1D2 99.97%)",
                 "linear-gradient(0.15deg, #E55D87 0.13%, #5FC3E4 99.88%)"
             ],
+            userData: {},
             tempGenres: new Map(),
             footerHeight: 0,
             termCount: 0,
@@ -99,10 +77,16 @@ class Callback extends React.Component {
         this.redirectToHome = this.redirectToHome.bind(this)
         this.updateTermCount = this.updateTermCount.bind(this)
         this.updateGenres = this.updateGenres.bind(this)
+        this.addToPlaylist = this.addToPlaylist.bind(this)
     }
 
     componentDidMount() {
         // set top tracks
+        window.mobileCheck = function() {
+            let check = false;
+            (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+            return check;
+          };
 
         const footerHeight = document.getElementById("footer").clientHeight
         this.setState({footerHeight: footerHeight})  
@@ -112,7 +96,17 @@ class Callback extends React.Component {
         const colorIdx = Math.floor(Math.random() * (max - min + 1)) + min
 
         document.body.style.backgroundImage = this.state.colors[colorIdx];
-        
+
+        superagent.get(`https://api.spotify.com/v1/me`)
+        .set("Authorization", "Bearer " + this.state.access_token)
+        .end((err, res) => {
+            if(err){
+                console.log(err)
+            } else {
+                console.log(res.body)
+                this.setState({userData: res.body})
+            }
+        })
         this.state.data.forEach((item, i) => {
             this.getTopItems("tracks", item.term, 50)
             .then((res) => {
@@ -159,6 +153,14 @@ class Callback extends React.Component {
         })
     }
 
+    componentWillUnmount(){
+        document.body.style.backgroundImage = null;
+    }
+
+    updateTermCount() {
+        this.setState({loading:true, termCount: this.state.termCount + 1}, this.updateValues);
+    }
+
     updateGenres(){
         this.state.data.forEach((item, i) => {
             
@@ -186,12 +188,52 @@ class Callback extends React.Component {
         })
     }
 
-    componentWillUnmount(){
-        document.body.style.backgroundImage = null;
-    }
+    addToPlaylist() {
+        const playlistName = this.state.data[this.state.termCount % 3].label.toLowerCase()
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        let yyyy = today.getFullYear();
 
-    updateTermCount() {
-        this.setState({loading:true, termCount: this.state.termCount + 1}, this.updateValues);
+        today = mm + '/' + dd + '/' + yyyy;
+        superagent.post(`https://api.spotify.com/v1/users/${this.state.userData.id}/playlists`)
+        .set("Authorization", "Bearer " + this.state.access_token)
+        .set('Content-Type', 'application/json')
+        .send({
+            name: `Your WrappedWhenever from ${playlistName}`,
+            description: `Created on ${today}`,
+            public: false
+        })
+        .end((err, res) => {
+            if(err) {
+                console.log(err)
+            } else {
+                if(res.created === true){
+                    console.log(res)
+
+                    const trackURIs = this.state.data[this.state.termCount % 3].tracks.map((track, i) => {
+                        return track.uri
+                    })
+
+                    console.log(trackURIs)
+
+                    superagent.post(`https://api.spotify.com/v1/playlists/${res.body.id}/tracks`)
+                    .set("Authorization", "Bearer " + this.state.access_token)
+                    .set('Content-Type', 'application/json')
+                    .send(trackURIs)
+                    .end((err, res) => {
+                        if(err) {
+                            console.log(err)
+                        } else {
+                            console.log(res.body)
+                        }
+                    })
+                } else {
+                    console.log("couldn't make playlist")
+                    console.log(res)
+                }
+            }
+        })
     }
 
     async getArtistInfo(artistId) {
@@ -250,57 +292,96 @@ class Callback extends React.Component {
                 </div> 
             )
         })
+
+        const styles = {
+            main_color: '#181818',
+            mobile_container: {
+                padding: 20
+            },
+            main_card: {
+                height: window.innerHeight - this.state.footerHeight - 40,
+                backgroundImage: "linear-gradient(360deg, #181818 0%, #343434 100%)",
+                borderRadius: 10
+            },
+            topHeading: {
+                fontWeight: 700
+            },
+            textItem: {
+                marginBottom: -4, 
+                fontSize: 12, 
+                maxWidth: 140
+            },
+            footerTag: {
+                marginBottom: 0, 
+                fontWeight:700, 
+                fontSize: 14
+            }
+        }
     
-        const main_card_height = window.innerHeight - this.state.footerHeight - 40;
         const main_color = '#181818'
         const data = this.state.data[this.state.termCount % 3]
+
+        let reactSwipeEl;
         return (
             <div>
                 <div className="container">
                     <div className="row">
-                        <div style={{padding:20}} className="col-sm d-md-none d-lg-none d-xl-none d-xxl-none d-lg-block d-xl-block">
-                            <div id="main" style={{backgroundImage: "linear-gradient(360deg, #181818 0%, #343434 100%)", height: main_card_height, borderRadius: 10}}>
-                                <div className="parent perspective" style={{marginLeft: 350}}>
-                                    {data.tracks.slice(0, 5).map((track, i) => {
-                                        
-                                        const offset = i * 40;
-                                        const index = 5 - i;
-                                        return(
-                                            <img key={i} alt={i} id="track" style={{right:offset, zIndex:index}} src={track.album.images[1].url} height="190" width="190" className="square-img child"/>
-                                        )
-                                    })}
+                        <div style={styles.mobile_container} className="col-sm d-md-none d-lg-none d-xl-none d-xxl-none d-lg-block d-xl-block">
+                            <ReactSwipe 
+                                className="carousel"
+                                swipeOptions={{continuous:false}}
+                                ref={el => (reactSwipeEl = el)}
+                            >
+                                <div id="main" style={styles.main_card}>
+                                    <div className="parent perspective" style={{marginLeft: 350}}>
+                                        {data.tracks.slice(0, 5).map((track, i) => {
+                                            
+                                            const offset = i * 40;
+                                            const index = 5 - i;
+                                            return(
+                                                <img key={i} alt={i} id="track" style={{right:offset, zIndex:index}} src={track.album.images[1].url} height="190" width="190" className="square-img child"/>
+                                            )
+                                        })}
+                                    </div>
+                                    <div className="container-sm">
+                                        <div className="row" style={{marginTop:210}}>
+                                            <div className="col">
+                                                <h6 style={styles.topHeading}>TOP ARTISTS</h6>
+                                                {data.artists.slice(0,5).map((artist, i) => {
+                                                    return(
+                                                        <div key={i}><p className="d-block text-truncate" style={styles.textItem}><b>{i + 1}</b>&nbsp;&nbsp;{artist.name}</p></div>
+                                                    )
+                                                })}
+                                            </div>
+                                            <div className="col">
+                                                <h6 style={styles.topHeading}>TOP SONGS</h6>
+                                                {data.tracks.slice(0,5).map((track, i) => {
+                                                    return(
+                                                        <div key={i}><p className="d-block text-truncate" style={styles.textItem}><b>{i + 1}</b>&nbsp;&nbsp;{track.name}</p></div>
+                                                    )
+                                                })}
+                                            </div>
+                                        </div>
+                                        <div className="row" style={{marginTop: 20}}>
+                                            <h6 style={{...styles.topHeading, textAlign:'center'}}>TOP GENRES</h6>
+                                            <div className="d-flex flex-column">
+                                                {Array.from(data.genres.keys()).slice(0,5).map((genre, i) => {
+                                                    return(
+                                                        <p key={i} className="d-sm-block text-truncate" style={styles.textItem}><b>{i + 1}</b>&nbsp;{genre}</p>
+                                                    )
+                                                })}
+                                            </div>
+                                        </div>
+                                    </div>                    
                                 </div>
-                                <div className="container-sm">
-                                    <div className="row" style={{marginTop:210}}>
-                                        <div className="col">
-                                            <h6 style={{fontWeight:700}}>TOP ARTISTS</h6>
-                                            {data.artists.slice(0,5).map((artist, i) => {
-                                                return(
-                                                    <div key={i}><p className="d-block text-truncate" style={{marginBottom: -4, fontSize: 12, maxWidth: 140}}><b>{i + 1}</b>&nbsp;&nbsp;{artist.name}</p></div>
-                                                )
-                                            })}
-                                        </div>
-                                        <div className="col">
-                                            <h6 style={{fontWeight:700}}>TOP SONGS</h6>
-                                            {data.tracks.slice(0,5).map((track, i) => {
-                                                return(
-                                                    <div key={i}><p className="d-block text-truncate" style={{marginBottom: -4, fontSize: 12, maxWidth: 140}}><b>{i + 1}</b>&nbsp;&nbsp;{track.name}</p></div>
-                                                )
-                                            })}
-                                        </div>
-                                    </div>
-                                    <div className="row" style={{marginTop: 20}}>
-                                        <h6 style={{fontWeight:700, textAlign:'center'}}>TOP GENRES</h6>
-                                        <div className="d-flex flex-column">
-                                            {Array.from(data.genres.keys()).slice(0,5).map((genre, i) => {
-                                                return(
-                                                    <p key={i} className="d-sm-block text-truncate" style={{marginBottom: -4, fontSize: 12}}><b>{i + 1}</b>&nbsp;{genre}</p>
-                                                )
-                                            })}
-                                        </div>
-                                    </div>
-                                </div>                    
-                            </div>
+
+                                <div id="main" style={styles.main_card}>
+                                    <div style={{height: '100%'}} className="d-flex align-items-end justify-content-center">
+                                        <button onClick={this.addToPlaylist} className="login-btn">Add to playlist</button>
+                                    </div>               
+                                </div>
+                            </ReactSwipe>
+                            
                         </div>
                         <div className="col-sm d-none d-sm-none d-sm-block d-md-block">
                             <div className="d-flex flex-row justify-content-between">
@@ -319,7 +400,7 @@ class Callback extends React.Component {
                 </div>
                 
                 <div id="footer" style={{backgroundColor: main_color}} className="fixed-bottom share-footer d-flex flex-row justify-content-between align-items-center">
-                    <p style={{marginBottom: 0, fontWeight:600, fontSize: 14}}>#WRAPPEDWHENEVER</p>
+                    <p style={styles.footerTag}>#WRAPPEDWHENEVER</p>
                     <a href="#" onClick={this.updateTermCount} className="term-select" style={{fontSize:14}}>{this.state.data[this.state.termCount % 3].label}</a>
                 </div>
             </div>
